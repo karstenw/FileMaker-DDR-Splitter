0b435e8636f5bd038bad9f474d8196a24e582ede
Karsten Wolf <karstenwo@web.de>
2015-04-12 23:50:49 +0200

    Initial.

---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+Script for building the FileMaker-DDR-Splitter application
+
+Usage:
+    python setup.py py2app -gx -O2
+"""
+import os
+
+from distutils.core import setup
+import py2app
+
+appname = "FileMaker DDR Splitter"
+appnameshort = "FMPSplit"
+version = "V0.1.0"
+
+copyright = u"Copyright 2015 Karsten Wolf"
+
+infostr = appname + u' ' + version + u' ' + copyright
+
+
+
+setup(
+    app=[{
+        'script': "FMPDDRSplitter.py",
+
+        'plist':{
+            'CFBundleGetInfoString': infostr,
+            'CFBundleIdentifier': 'org.kw.ddrsplitter',
+            'CFBundleShortVersionString': version,
+            'CFBundleDisplayName': appnameshort,
+            'CFBundleName': appnameshort,
+            'CFBundleSignature': 'KWDs',
+            'LSHasLocalizedDisplayName': False,
+            'NSAppleScriptEnabled': False,
+            'NSHumanReadableCopyright': copyright}}],
+
+    data_files=["English.lproj/MainMenu.nib",
+                "Icon.icns"],
+    options={
+        'py2app':{
+            'iconfile': "Icon.icns",
+        },
+    },
+)
